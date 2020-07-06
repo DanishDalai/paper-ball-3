98eae388d37fe74e45f7e15e987e4604fd8a7fab
DanishDalai <65957236+DanishDalai@users.noreply.github.com>
2020-07-06 20:31:47 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,41 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var paper,slingShot;
+
+ var gameState = "onSling";
+
+function setup() {
+ var canvas = createCanvas(1200,400);
+  engine = Engine.create();
+  world = engine.world;
+  
+  paper = new Ball(100,200,60,60);
+  ground = new Ground(600,height,1200,20)
+  box = new Box(800,315,150,150)
+  slingShot = new SlingShot(paper.body,{x:100,y:200})
+}
+
+function draw() {
+  background(255);  
+  Engine.update(engine);
+  strokeWeight(4);
+  paper.display();
+  box.display();
+  ground.display();
+  slingShot.display();
+}
+
+function mouseDragged(){
+  if (gameState!=="launched"){
+      Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+  }
+}
+
+
+function mouseReleased(){
+  slingshot.fly();
+  gameState = "launched";
+}
